fad1a5d7d2d21e21e00a05d2f008e59fde34759e
Airsoft102 <league@WTS-IM-6.local>
2018-08-14 12:00:13 -0700

    Great day 2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -4,12 +4,27 @@ import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
-	
+
 	public static void main(String[] args) {
-		
+
 		// START HERE
-	
-		
-		
+		Robot Froome = new Robot("batman");
+		Froome.setSpeed(200);
+		Froome.penDown();
+		Froome.move(200);
+		Froome.turn(90);
+		Froome.move(200);
+		Froome.turn(90);
+		Froome.move(200);
+		Froome.turn(90);
+		Froome.move(200);
+		Froome.penUp();
+		Froome.move(200);
+Froome.penDown();
+		for (int i = 0; i < 4; i++) {
+			Froome.move(200);
+			Froome.turn(90);
+		}
+
 	}
 }
